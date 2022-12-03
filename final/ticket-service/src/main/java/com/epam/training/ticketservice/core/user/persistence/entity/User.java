0cb6d62368fbedda28d42f255277ad8644207575
mategordos <mategordos@gmail.com>
2022-12-04 00:06:25 +0100

    Fixed checkstyle grade 3 should work

--- final/ticket-service/src/main/java/com/epam/training/ticketservice/core/user/persistence/entity/User.java
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/core/user/persistence/entity/User.java
@@ -1,14 +1,25 @@
 package com.epam.training.ticketservice.core.user.persistence.entity;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.RequiredArgsConstructor;
+
 import org.hibernate.Hibernate;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Column;
+import javax.persistence.Enumerated;
+import javax.persistence.EnumType;
 import java.util.Objects;
 
 @Entity
 @Table(name = "Users")
-@Getter @Setter @RequiredArgsConstructor
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class User {
 
     @Id
